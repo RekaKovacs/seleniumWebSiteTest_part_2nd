ab2ec903645563993325ffab63d4991abbeaeb19
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2019-02-12 10:49:16 +0100

    add test dictionary to the same package and class and all fields are changed to package private in the "Elementslib" class.

--- src/com/codecool/selenium/testKwDDPOM/ElementsLib.java
+++ src/com/codecool/selenium/testKwDDPOM/ElementsLib.java
@@ -2,13 +2,13 @@ package com.codecool.selenium.testKwDDPOM;
 
 import org.openqa.selenium.By;
 
-public class ElementsLib {
-    public String baseURL = "https://www.seleniumeasy.com/test/";
-    public String textForInputField = "Hello world!";
+class ElementsLib {
+    String baseURL = "https://www.seleniumeasy.com/test/";
+    String textForInputField = "Hello world!";
 
-    public By selectInputFormsMenuList = By.xpath("//*[@id=\"treemenu\"]//*[contains(text(),'Input Forms')]");
-    public By selectSimpleFormDemoMenuList = By.xpath("//*[@id=\"treemenu\"]//*[contains(text(),'Simple Form Demo')]");
-    public By inputSingleInputField = By.xpath("//*[@id=\"user-message\"]");
-    public By buttonShowMessage = By.xpath("//*[@id=\"get-input\"]/button");
-    public By locationTypedText = By.xpath("//*[@id=\"display\"]");
+    By selectInputFormsMenuList = By.xpath("//*[@id=\"treemenu\"]//*[contains(text(),'Input Forms')]");
+    By selectSimpleFormDemoMenuList = By.xpath("//*[@id=\"treemenu\"]//*[contains(text(),'Simple Form Demo')]");
+    By inputSingleInputField = By.xpath("//*[@id=\"user-message\"]");
+    By buttonShowMessage = By.xpath("//*[@id=\"get-input\"]/button");
+    By locationTypedText = By.xpath("//*[@id=\"display\"]");
 }
